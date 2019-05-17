700435714e696176c69d6010a52211838ef63bf3
Krzysztof Świerczek <dev.krzysiek@gmail.com>
2019-05-17 19:17:27 +0200

    initial commit

---
+++ CurrencyChecker/CurrencyChecker/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace CurrencyChecker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : Xamarin.Forms.TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
