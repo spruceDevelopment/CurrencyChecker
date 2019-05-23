8487f05b22c232689488b9a3a6aa1ea2004e87cf
Krzysztof Świerczek <dev.krzysiek@gmail.com>
2019-05-23 22:03:05 +0200

    android release parameters

--- CurrencyChecker.Core/Helpers/TaskUtilities.cs
+++ CurrencyChecker.Core/Helpers/TaskUtilities.cs
@@ -8,9 +8,7 @@ namespace CurrencyChecker.Core.Helpers
 {
     public static class TaskUtilities
     {
-#pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
         public static async void FireAndForgetSafeAsync(this Task task, IErrorHandler? handler = null)
-#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
         {
             try
             {
--- CurrencyChecker.Test/RhinoExtensions.cs
+++ CurrencyChecker.Test/RhinoExtensions.cs
@@ -9,9 +9,6 @@ namespace CurrencyChecker.Core.Test
 {
     public static class RhinoExtensions
     {
-        /// <summary>
-        /// Clears the behavior already recorded in a Rhino Mocks stub.
-        /// </summary>
         public static void ClearBehavior<T>(this T stub)
         {
             stub.BackToRecord(BackToRecordOptions.All);
